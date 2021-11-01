63e6e07dcff70b3c27ed70150d9ff4b25f0fa47b
Jennifer Robert <jrobert2121@gmail.com>
2021-11-01 17:10:59 -0400

    add mentorship eligibility and csv

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -28,3 +28,23 @@ INTO Retiring_Titles
 FROM Unique_Titles AS ut
 GROUP BY ut.title
 ORDER BY COUNT (ut.emp_no) DESC;
+
+--Create Membership Eligibility table of employees eligible for mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO Mentorship_Eligibility
+FROM employees as e
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+WHERE (de.to_date = ('9999-01-01'))
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
+	
+	
